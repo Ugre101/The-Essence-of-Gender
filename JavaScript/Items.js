7f79372530473d978e4045b130a74cce5dc7c2e3
Ugre101 <44323564+Ugre101@users.noreply.github.com>
2018-11-23 16:29:19 +0100

    Add files via upload

--- JavaScript/Items.js
+++ JavaScript/Items.js
@@ -1,4 +1,3 @@
-
 // default items. You can still create others if they respect the template
 const ItemDict = {
     itemTemplate: {
@@ -8,14 +7,16 @@ const ItemDict = {
         Does: "Short desc.",
         Quantity: 1,
         Title: "Long description of the item",
-        Use: function(who){EventLog(who.Name + " used the item")}
+        Use: function (who) {
+            EventLog(who.Name + " used the item")
+        }
     },
     orcCum: {
         Name: "Orc cum",
-        Use: function(who){
+        Use: function (who) {
             who.Masc += 50;
             //if (who == who.
-            EventLog("After drinking the orc cum, "+who.Name+" absorbs the manly essence of it.");
+            EventLog("After drinking the orc cum, " + who.Name + " absorbs the manly essence of it.");
         },
         Equip: "No",
         Drop: "Yes",
@@ -24,9 +25,9 @@ const ItemDict = {
     },
     fairyDust: {
         Name: "Fairy dust",
-        Use: function(who) {
+        Use: function (who) {
             who.Height -= 5;
-            EventLog("Inhaling the fairy dust you see the world grow before you, or maybe it's you who became shorter?");    
+            EventLog("Inhaling the fairy dust you see the world grow before you, or maybe it's you who became shorter?");
         },
         Use: "Yes",
         Equip: "No",
@@ -36,7 +37,7 @@ const ItemDict = {
     },
     humanity: {
         Name: "Humanity",
-        Use: function(who) {
+        Use: function (who) {
             TfEngine("human");
             EventLog("You try to regain your humanity...");
         },
@@ -47,7 +48,7 @@ const ItemDict = {
     },
     pouch: {
         Name: "Pouch",
-        Use: function(who) {
+        Use: function (who) {
             var z = Math.round(Math.random() * 40) + 10;
             z = parseInt(z);
             who.Gold += z;
@@ -60,7 +61,7 @@ const ItemDict = {
     },
     halfPouch: {
         Name: "Small Pouch",
-        Use: function(who) {
+        Use: function (who) {
             var z = Math.round(Math.random() * 40) + 10;
             z = parseInt(z);
             who.Gold += z;
@@ -73,7 +74,7 @@ const ItemDict = {
     },
     orcBrew: {
         Name: "Orc brew",
-        Use: function(who) {
+        Use: function (who) {
             var z = Math.min(Math.round(who.MaxHealth / 10), who.MaxHealth - who.Health);
             who.Health += z;
             EventLog("Bottoms up!");
@@ -83,10 +84,10 @@ const ItemDict = {
                 EventLog("Bleh. That ruined your night's sleep.");
         },
         Title: "Heals?"
-    }, 
+    },
     trollMilk: {
         Name: "Troll Milk",
-        Use: function(who) {
+        Use: function (who) {
             var z = Math.min(Math.round(who.MaxWillHealth / 10), who.MaxWillHealth - who.WillHealth);
             who.WillHealth += z;
             EventLog("Bottoms up!");
@@ -99,10 +100,10 @@ const ItemDict = {
         Drop: "Yes",
         Does: "Restores 10% of willpower.",
         Title: "Smells like it's already gone sour. Increases focus."
-    }, 
+    },
     elvenHair: {
         Name: "Elven hair",
-        Use: function(who) {
+        Use: function (who) {
             TfEngine("elf");
             EventLog("You try to become an elf...");
         },
@@ -110,10 +111,10 @@ const ItemDict = {
         Drop: "Yes",
         Does: "Elf TF",
         Title: "This is all you need to become an elf, no potions needed."
-    }, 
+    },
     amazonGirdle: {
         Name: "Amazon's Girdle",
-        Use: function(who) {
+        Use: function (who) {
             if (who.Masc > who.Femi + 100) {
                 who.Masc -= 50;
                 who.Femi += 50;
@@ -131,10 +132,10 @@ const ItemDict = {
         Drop: "Yes",
         Does: "Balances gender",
         Title: "Brings your essences closer together in strength."
-    }, 
+    },
     milkJug: {
         Name: "Milk Jug",
-        Use: function(who) {
+        Use: function (who) {
             for (var i = 0; i < who.Boobies.length; i++) {
                 who.Boobies[i].MilkRate += 0.1;
             }
@@ -144,10 +145,10 @@ const ItemDict = {
         Drop: "Yes",
         Does: "Increases lactation",
         Title: "It'll go straight to your chest!"
-    }, 
+    },
     fertilityIdol: {
         Name: "Fertility idol",
-        Use: function(who) {
+        Use: function (who) {
             who.Femi += 100;
             EventLog("You absorb the statue's latent energies.");
         },
@@ -155,10 +156,10 @@ const ItemDict = {
         Drop: "Yes",
         Does: "Increases femininity",
         Title: "Brings out your feminine side."
-    }, 
+    },
     cockyRock: {
         Name: "Cocky rock",
-        Use: function(who) {
+        Use: function (who) {
             who.Masc += 100;
             EventLog("You absorb the rock's male essence.");
         },
@@ -166,10 +167,10 @@ const ItemDict = {
         Drop: "Yes",
         Does: "Increases masculinity",
         Title: "Brings out your masculine side."
-    }, 
+    },
     infernalSemen: {
         Name: "Infernal semen",
-        Use: function(who) {
+        Use: function (who) {
             who.Masc += who.Femi;
             who.Femi = 0;
             EventLog("The infernally-hot semen burns your feminine essence, leaving you 100% male.");
@@ -178,10 +179,10 @@ const ItemDict = {
         Drop: "Yes",
         Does: "Turns femininity into masculinity",
         Title: "Turns your feminine side into another masculine one."
-    }, 
+    },
     infernalMilk: {
         Name: "Infernal milk",
-        Use: function(who) {
+        Use: function (who) {
             who.Femi += who.Masc;
             who.Masc = 0;
             EventLog("The infernally-hot milk burns your masculine essence, leaving you 100% female.");
@@ -191,106 +192,84 @@ const ItemDict = {
         Does: "Turns masculinity into femininity",
         Title: "Turns your masculine side into another feminine one."
     },
-	    SuccMilk: {
+    SuccMilk: {
         Name: "Milk+",
-        Use: function(who) {
-			var a = "", 
-			    b = "";
-            if(Settings.EssenceAuto == false && who.Boobies.length < 2)
-			{
-				if(who.Boobies[0].Size < 6)
-				{
-					who.Boobies[0].Size = 6;
-					a = "your breasts swell";
-				}
-				who.Boobies[1] = who.Boobies[0];
-				a += ", as another pair grow right below them to the same size!";
-			}
-			else if(Settings.EssenceAuto == false)
-			{
-				var c = 0;
-				for(var i = 0; i < who.Boobies.length; i++)
-				{
-					if(who.Boobies[i].Size < 6)
-					{
-						who.Boobies[i].Size = 6;
-						c++;
-					}
-				}
-				if (c > 0)
-					a = "your smallest breasts swell to D-cups!";
-			}
-			else if(who.Femi <= 3560)
-			{
-				who.Femi = 3561;
-				a = "your chest explodes with activity, leaving you with a feminized body, complete with massive breasts!";
-			}
-			else a = "your boobs vibrate with energy...";
-			FluidsEngine();
-			for(var i = 0; i < who.Boobies.length; i++)
-			{
-				if(who.Boobies[i].Milk < who.Boobies[i].MilkMax)
-				{
-				who.Boobies[i].Milk = who.Boobies[i].MilkMax;
-				b = "<br>A few seconds later, they're completely filled with milk!";
-				}
-			}
-			
+        Use: function (who) {
+            var a = "",
+                b = "";
+            if (Settings.EssenceAuto == false && who.Boobies.length < 2) {
+                if (who.Boobies[0].Size < 6) {
+                    who.Boobies[0].Size = 6;
+                    a = "your breasts swell";
+                }
+                who.Boobies[1] = who.Boobies[0];
+                a += ", as another pair grow right below them to the same size!";
+            } else if (Settings.EssenceAuto == false) {
+                var c = 0;
+                for (var i = 0; i < who.Boobies.length; i++) {
+                    if (who.Boobies[i].Size < 6) {
+                        who.Boobies[i].Size = 6;
+                        c++;
+                    }
+                }
+                if (c > 0)
+                    a = "your smallest breasts swell to D-cups!";
+            } else if (who.Femi <= 3560) {
+                who.Femi = 3561;
+                a = "your chest explodes with activity, leaving you with a feminized body, complete with massive breasts!";
+            } else a = "your boobs vibrate with energy...";
+            FluidsEngine();
+            for (var i = 0; i < who.Boobies.length; i++) {
+                if (who.Boobies[i].Milk < who.Boobies[i].MilkMax) {
+                    who.Boobies[i].Milk = who.Boobies[i].MilkMax;
+                    b = "<br>A few seconds later, they're completely filled with milk!";
+                }
+            }
+
             EventLog("Feeling a flush of heat in your chest, you glance down just in time to see " + a + b);
-			FluidsEngine();
+            FluidsEngine();
         },
         Equip: "No",
         Drop: "Yes",
         Does: "Makes you filled with milk!",
         Title: "Guarantees milkiness."
     },
-	    IncSemen: {
+    IncSemen: {
         Name: "Semen+",
-        Use: function(who) {
-			var a = "", 
-			    b = "";
-            if(Settings.EssenceAuto == false && who.Balls.length < 2)
-			{
-				a = "your balls"
-				if(who.Balls[0].Size < 6)
-				{
-					who.Balls[0].Size = 6;
-					a += " swell";
-				}
-				who.Balls[1] = who.Balls[0];
-				a += ", as another pair grow right beside them to the same size!";
-			}
-			else if(Settings.EssenceAuto == false)
-			{
-				var c = 0;
-				for(var i = 0; i < who.Balls.length; i++)
-				{
-					if(who.Balls[i].Size < 6)
-					{
-						who.Balls[i].Size = 6;
-						c++;
-					}
-				}
-				if (c > 0)
-					a = "your smallest balls swell in size!";
-			}
-			else if(who.Masc <= 3560)
-			{
-				who.Masc = 3561;
-				a = "your groin explode with activity, leaving you with a masculinized body, complete with massive dicks and balls!";
-			}
-			else a = "your balls vibrate with energy...";
-			FluidsEngine();
-			for(var i = 0; i < who.Balls.length; i++)
-			{
-				if(who.Balls[i].Cum < who.Balls[i].CumMax)
-				{
-				who.Balls[i].Cum = who.Balls[i].CumMax;
-				b = " A few seconds later, they're completely filled with cum!";
-				}
-			FluidsEngine();
-			}
-			
+        Use: function (who) {
+            var a = "",
+                b = "";
+            if (Settings.EssenceAuto == false && who.Balls.length < 2) {
+                a = "your balls"
+                if (who.Balls[0].Size < 6) {
+                    who.Balls[0].Size = 6;
+                    a += " swell";
+                }
+                who.Balls[1] = who.Balls[0];
+                a += ", as another pair grow right beside them to the same size!";
+            } else if (Settings.EssenceAuto == false) {
+                var c = 0;
+                for (var i = 0; i < who.Balls.length; i++) {
+                    if (who.Balls[i].Size < 6) {
+                        who.Balls[i].Size = 6;
+                        c++;
+                    }
+                }
+                if (c > 0)
+                    a = "your smallest balls swell in size!";
+            } else if (who.Masc <= 3560) {
+                who.Masc = 3561;
+                a = "your groin explode with activity, leaving you with a masculinized body, complete with massive dicks and balls!";
+            } else a = "your balls vibrate with energy...";
+            FluidsEngine();
+            for (var i = 0; i < who.Balls.length; i++) {
+                if (who.Balls[i].Cum < who.Balls[i].CumMax) {
+                    who.Balls[i].Cum = who.Balls[i].CumMax;
+                    b = " A few seconds later, they're completely filled with cum!";
+                }
+                FluidsEngine();
+            }
+
             EventLog("Feeling a flush of heat in your groin, you glance down just in time to see " + a + b);
         },
         Equip: "No",
@@ -298,11 +277,11 @@ const ItemDict = {
         Does: "Makes you filled with cum!",
         Title: "Guarantees cum-filling."
     },
-	    book: {
+    book: {
         Name: "Book",
-        Use: function(who) {
-			who.Int++;
-			who.Arousal = 99;
+        Use: function (who) {
+            who.Int++;
+            who.Arousal = 99;
             EventLog("A dirty magazine, that's made you extremely aroused...");
         },
         Equip: "No",
@@ -322,13 +301,16 @@ const ItemDict = {
     },
     Milker: {
         Name: "Milker",
-        Use: function(who) {
+        Use: function (who) {
+            var Milktotal = 0;
             for (var e of player.Boobies) {
                 if (e.Milk > 0) {
+                    Milktotal += e.Milk;
                     e.Milk = 0
-                    
                 }
             }
+            var bottles = Math.round(Milktotal) //1L per bottle
+            SnowInventoryAdd(ItemDict.MilkBottle, bottles);
         },
         Equip: "No",
         Drop: "Yes",
@@ -337,19 +319,30 @@ const ItemDict = {
     },
     MilkBottle: {
         Name: "Milk",
-        Use: function(who) {
+        Use: function (who) {
+            var a = RandomInt(1,20);
+            if (a > 18) {
+                for (var e of player.Boobies) {
+                    e.MilkRate++;
+                    EventLog("After drinking the last of the milk you get a funny feeling in your chest.")
+                }
+            }
+            player.Health += 20;
+            player.WillHealth += 20;
+            player.Fat += 0.2;
             //Drink milk
         },
         Equip: "No",
         Drop: "Yes",
         Does: "...",
-        Title: "A bottle of milk."
+        Title: "A 1L bottle of milk."
     },
     PocketPortal: {
         Name: "Pocket portal",
-        Use: function(who) {
-            console.log(House)
-            if (House.Portal) {
+        Use: function (who) {
+            if (player.Map === "RoadToHome") {
+                document.getElementById("InventoryText").innerHTML = "..."
+            } else if (House.Portal) {
                 who.Area = "First";
                 who.Map = "RoadToHome";
                 DisplayGame();
@@ -360,7 +353,7 @@ const ItemDict = {
         },
         Equip: "No",
         Does: "Teleport home",
-        Drop: "No",//I think?
+        Drop: "No", //I think?
         Title: "Never walk home again!"
     }
 }
--- JavaScript/Mountain.js
+++ JavaScript/Mountain.js
@@ -1,22 +1,50 @@
-var MountainHalfRaces = ["human", "elf"]//
-var MountainRaces = ["dragon", "giant", "human", "harpy",""]
+var MountainHalfRaces = ["human", "elf"] //
+var MountainRaces = ["dragon", "giant", "human", "harpy", ""]
 
 // Mountain opponents
 // "Pure" Maiden
 function EncounterMaiden() {
-    var OP = new enemy("Pure", "temp", 0, RandomInt(1000, 10000), RandomInt(1, 10), RandomIn(1, 10), RandomInt(20, 50),
-    RandomInt(0, 5), RandomInt(5, 20), RandomInt(0, 5), 0 , 0, 2500, 2500, 5000, 5000, RandomInt(2, 18) * grid, 
-    RandomInt(2, 18) * grid, RandomInt(50, 250), RandomInt(30, 100), "Pink", grid, RandomInt(40, 60), RandomInt(150, 170), 
-    RandomInt(10, 20), RandomInt(10, 15));
+    var OP = new enemy("Pure", MountainHalfRaces, RandomInt(1, 10), RandomIn(1, 10), RandomInt(20, 50), RandomInt(0, 5),
+        RandomInt(5, 20), RandomInt(0, 5), 2500, 2500, 5000, 5000, RandomInt(50, 250), RandomInt(30, 100),
+        "Pink", grid, RandomInt(150, 170));
+    EssenceGiver(OP, 1500, "female");
+    FatMuscle(OP, 1, 1);
+    StandardEnemy(OP);
+    NameGiver(OP);
+    return OP;
     /* Strong virgins, they are not virgin by choice it's their culture to not offer themself to anyone weaker
     which have made them very strong over generation but there is a lot of desire to be dominated.
-    After beating them change their title to depraved or something.*/ 
+    After beating them change their title to depraved or something.*/
     
 }
 
 function EncounterDragonKind() {
-    var OP = new enemy("Lesser", "dragon", RandomInt(1000, 5000), RandomInt(1000, 5000), RandomInt(10,50), RandomInt(10, 50),
-    RandomInt(10, 30), RandomInt(30, 50), RandomInt(10, 50), RandomInt(10, 60), 0, 0, 5000, 5000, 5000, 5000,
-    RandomInt(2, 18) * grid, RandomInt(2,18)* grid, RandomInt(50, 500), RandomInt(50, 500), "Red", 
-    grid, RandomInt(60,120), RandomInt(160,240), RandomInt(10, 40), RandomInt(5,20), RandomString(MountainHalfRaces)); 
+    var OP = new enemy("Lesser", "Dragon", RandomInt(10, 50), RandomInt(10, 50), RandomInt(10, 30), RandomInt(30, 50),
+        RandomInt(10, 50), RandomInt(10, 60), 5000, 5000, 5000, 5000, RandomInt(50, 500), RandomInt(50, 500),
+        "Red", grid, RandomInt(160, 240), RandomString(MountainHalfRaces));
+    EssenceGiver(OP, 1500);
+    FatMuscle(OP, 1, 1);
+    StandardEnemy(OP);
+    NameGiver(OP);
+    return OP;
+}
+
+function EncounterHarpy() {
+    var OP = new enemy("Young", "Harpy", RandomInt(10, 30), RandomInt(30, 70), RandomInt(0, 20), RandomInt(10, 60),
+        RandomInt(0, 10), RandomInt(20, 80), 6000, 6000, 4500, 4500, RandomInt(50, 500), RandomInt(50, 500),
+        "Yellow", grid, RandomInt(70, 140));
+    EssenceGiver(OP, 1500);
+    FatMuscle(OP, 1, 1);
+    StandardEnemy(OP);
+    NameGiver(OP);
+    return OP;
+}
+
+function EncounterAnthroDragon() {
+    var OP = new enemy("Young", "Dragon");
+    EssenceGiver(OP, 1500);
+    FatMuscle(OP, 1, 1);
+    StandardEnemy(OP);
+    NameGiver(OP);
+    return OP;
 }
