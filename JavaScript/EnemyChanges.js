9b3f3e2ddda2c4f2ca4ce63a560a9f726282ccde
Ugre101 <44323564+Ugre101@users.noreply.github.com>
2018-12-03 17:17:51 +0100

    Add files via upload

---
+++ JavaScript/EnemyChanges.js
@@ -0,0 +1,15 @@
+function EEChanges(ee) {
+    switch (ee.Name) {
+        case "Pure":
+            ee.Name = "Depraved"
+            break;
+        default:
+            break;
+    }
+    switch (ee.Race) {
+        default:
+            break;
+    }
+}
+/* Not in use for the moment but I made it so that if I add more scenarios like pure maiden, it's easy 
+    to expand on*/
